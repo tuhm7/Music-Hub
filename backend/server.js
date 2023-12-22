6782c286562742afdbb8bd8a0d18275721bff4a0
Taesung Uhm <tuhm@Taesungs-MacBook-Pro.local>
2023-12-22 21:25:33 +0900

    apple playlist scraper

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,31 @@
 const express = require("express");
+const axios = require("axios");
+const cheerio = require("cheerio");
+const request = require("request");
+const querystring = require("querystring");
+require("dotenv").config();
 
 const app = express();
-const PORT = 4000;
 
 app.get("/", (req, res) => {
-  res.send("<h1>initial commit</h1>");
+  getApplePlaylist("https://music.apple.com/us/artist/my-playlist/1253909570");
+  res.send("filler");
 });
 
-app.listen(PORT, (error) => {
+async function getApplePlaylist(url) {
+  const response = await axios.get(url);
+  data = response.data;
+  const $ = await cheerio.load(data);
+  script = $("script").text();
+  script_trimmed = script
+    .substring(0, script.indexOf("import.meta.url"))
+    .trim();
+  object = JSON.parse(script_trimmed);
+  console.log(object["tracks"]);
+}
+app.listen(process.env.PORT, (error) => {
   if (!error) {
-    console.log("Server is running on port " + PORT);
+    console.log("Server is running on port " + process.env.PORT);
   } else {
     console.log("Error occurred", error);
   }

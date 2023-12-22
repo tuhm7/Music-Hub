6ba62d5f4dba08288a4a54e9bf3e255714a26a5e
Taesung Uhm <tuhm@Taesungs-MacBook-Pro.local>
2023-12-22 22:54:08 +0900

    spotify auth

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,66 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 const request = require("request");
 const querystring = require("querystring");
+const cors = require("cors");
+const randomstring = require("randomstring");
 require("dotenv").config();
 
+client_id = process.env.CLIENT_ID;
+client_secret = process.env.CLIENT_SECRET;
+
 const app = express();
+app.use(cors());
 
 app.get("/", (req, res) => {
   getApplePlaylist("https://music.apple.com/us/artist/my-playlist/1253909570");
   res.send("filler");
+});
+
+app.get("/login", function (req, res) {
+  var state = randomstring.generate(16);
+  var scope = "user-read-private user-read-email";
+
+  res.redirect(
+    "https://accounts.spotify.com/authorize?" +
+      querystring.stringify({
+        response_type: "code",
+        client_id: client_id,
+        scope: scope,
+        redirect_uri: process.env.REDIRECT_URI,
+        state: state,
+      })
+  );
+});
+
+app.get("/callback", function (req, res) {
+  var code = req.query.code || null;
+  var state = req.query.state || null;
+
+  if (state === null) {
+    res.redirect(
+      "/#" +
+        querystring.stringify({
+          error: "state_mismatch",
+        })
+    );
+  } else {
+    var authOptions = {
+      url: "https://accounts.spotify.com/api/token",
+      form: {
+        code: code,
+        redirect_uri: process.env.REDIRECT_URI,
+        grant_type: "authorization_code",
+      },
+      headers: {
+        "content-type": "application/x-www-form-urlencoded",
+        Authorization:
+          "Basic " +
+          new Buffer.from(client_id + ":" + client_secret).toString("base64"),
+      },
+      json: true,
+    };
+    res.redirect("/");
+  }
 });
 
 async function getApplePlaylist(url) {

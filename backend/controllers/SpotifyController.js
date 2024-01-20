5664e16bf425cfc9e8168d93e50e28ffaadbe1ac
Taesung Uhm <tuhm@lawn-128-61-35-104.lawn.gatech.edu>
2024-01-20 18:08:08 -0500

    create empty playlist on user spotify account

--- backend/controllers/SpotifyController.js
+++ backend/controllers/SpotifyController.js
@@ -2,7 +2,7 @@ const querystring = require("querystring");
 const { redisClient } = require("../helpers/init_redis");
 
 function createPlaylist(req, res) {
-  playlist = req.body["playlist"].slice(0, 10);
+  playlist = req.body["playlist"];
   redisClient.get("sess:" + req.sessionID).then((resp) => {
     resp = JSON.parse(resp);
     const token = resp["token"];
@@ -12,11 +12,12 @@ function createPlaylist(req, res) {
     const spotifyPlaylist = Promise.all(promises);
     spotifyPlaylist.then((data) => res.send(data));
   });
+  createEmptySpotifyPlaylist(req.session.spotifyID, req.session.token);
 }
 
 async function getSong(title, access_token) {
   spotifyPlaylist = [];
-  var options = {
+  const options = {
     headers: { Authorization: "Bearer " + access_token },
     json: true,
   };
@@ -37,5 +38,28 @@ async function getSong(title, access_token) {
       };
     });
 }
+
+function createEmptySpotifyPlaylist(user_id, access_token) {
+  console.log(user_id);
+  endpoint = "https://api.spotify.com/v1/users/" + user_id + "/playlists";
+  const options = {
+    method: "POST",
+    headers: {
+      Authorization: "Bearer " + access_token,
+      "Content-Type": "application/json",
+    },
+    json: true,
+    body: JSON.stringify({
+      name: "playlist",
+      description: "testing",
+      public: false,
+    }),
+  };
+  fetch(endpoint, options)
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+}
+
+function addSongsToPlaylist(playlistURI, songsList) {}
 
 module.exports = { createPlaylist };
